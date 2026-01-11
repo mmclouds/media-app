d13bd4045ad39150f119b612de238bbbe1eab0cd
wangsensnow@163.com <wangsensnow@163.com>
2026-01-11 16:46:25 +0800

    修改dashboard页面

--- src/app/[locale]/(protected)/dashboard/page.tsx
+++ src/app/[locale]/(protected)/dashboard/page.tsx
@@ -5,38 +5,40 @@ import { SectionCards } from '@/components/dashboard/section-cards';
 import { useTranslations } from 'next-intl';
 
 import data from './data.json';
-
+import { Routes } from '@/routes';
+import { redirect } from 'next/navigation';
 /**
  * Dashboard page
  *
  * NOTICE: This is a demo page for the dashboard, no real data is used,
- * we will show real data in the future
+ * 重定向到setting页面
  */
 export default function DashboardPage() {
-  const t = useTranslations();
+  // const t = useTranslations();
 
-  const breadcrumbs = [
-    {
-      label: t('Dashboard.dashboard.title'),
-      isCurrentPage: true,
-    },
-  ];
+  // const breadcrumbs = [
+  //   {
+  //     label: t('Dashboard.dashboard.title'),
+  //     isCurrentPage: true,
+  //   },
+  // ];
 
-  return (
-    <>
-      <DashboardHeader breadcrumbs={breadcrumbs} />
+  // return (
+  //   <>
+  //     <DashboardHeader breadcrumbs={breadcrumbs} />
 
-      <div className="flex flex-1 flex-col">
-        <div className="@container/main flex flex-1 flex-col gap-2">
-          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <SectionCards />
-            <div className="px-4 lg:px-6">
-              <ChartAreaInteractive />
-            </div>
-            <DataTable data={data} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  //     <div className="flex flex-1 flex-col">
+  //       <div className="@container/main flex flex-1 flex-col gap-2">
+  //         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+  //           <SectionCards />
+  //           <div className="px-4 lg:px-6">
+  //             <ChartAreaInteractive />
+  //           </div>
+  //           <DataTable data={data} />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
+  redirect(Routes.SettingsProfile);
 }
